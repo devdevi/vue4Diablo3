7aea92b1d33fe73859f968e3c40f901624e0d814
Chaves R <visaka.chaves@consorcio.cl>
2020-06-03 22:57:09 -0400

    router in main file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+/* import router from './router' */
 import store from './store'
 
 // BootstrapVue
@@ -10,23 +10,59 @@ import './plugins/bootstrapVue'
 import './plugins/fontAwesome'
 import './assets/css/main.styl'
 
-Vue.config.productionTip = false
+import Router from 'vue-router'
+import axios from 'axios'
 
-new Vue({
-  router,
-  store,
-  methods: {
-    // Nuestra función
-    init () {
-      console.log('Hola 🌝')
-      /* store.dispatch('oauth/getToken', null, { root: true }) */
-      store.dispatch('oauth/getToken')
+Vue.config.productionTip = false
+Vue.use(Router)
+const url = 'https://cns.modyo.cloud/api/content/spaces/landing-oncologico/types/form-apv/entries/cabd741d-6042-48e2-8820-98fa61234158'
+
+axios.get(url)
+  .then(data => {
+    const routerOptions = [
+      { path: '/', name: 'Home' },
+      { path: '/region/:region/profile/:battleTag', name: 'Profile' },
+      { path: '/region/:region/profile/:battleTag/hero/:heroId', name: 'Hero' },
+      { path: '/error', name: 'Error' },
+      { path: '*', redirect: { name: 'Home' } }
+    ]
+    const { path, name } = data.data.fields
+    const route = {
+      path,
+      name
     }
-  },
-  // Hook created
-  created () {
-    this.init()
-    console.log(router)
-  },
-  render: h => h(App)
-}).$mount('#app')
+    routerOptions.push(route)
+
+    const routes = routerOptions.map(r => {
+      return {
+        ...r,
+        // Lazy load
+        component: () => import(/* webpackChunkName: "[request]" */`@/views/${r.name}/Index.vue`)
+      }
+    })
+
+    console.log(routes)
+    const router = new Router({
+      routes
+    })
+
+    // Rutas
+    new Vue({
+      router,
+      store,
+      methods: {
+        // Nuestra función
+        init () {
+          console.log('Hola 🌝')
+          /* store.dispatch('oauth/getToken', null, { root: true }) */
+          store.dispatch('oauth/getToken')
+        }
+      },
+      // Hook created
+      created () {
+        console.log(router)
+        this.init()
+      },
+      render: h => h(App)
+    }).$mount('#app')
+  })
